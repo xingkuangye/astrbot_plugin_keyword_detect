e03454ef41b4f09e32a27c82bac16a4d7253d3cc
wuyufeng9960 <wyf091020@icloud.com>
2026-01-09 16:25:31 +0800

    init metadata

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("keyword_detect", "星星旁の旷野", "识别指定关键字并将消息转发至指定群聊", "0.3.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
